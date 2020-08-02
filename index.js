0cec3cd85d1c03dc7be873379c24d202024e9e37
kimi532 <kimi532@naver.com>
2020-08-02 17:24:18 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const {User} = require("./models/User")
+
+const config =require('./config/key');
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://imakeit:ekzjtm0883^^@cluster0.y5bhb.mongodb.net/<dbname>?retryWrites=true&w=majority',{
-  useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:false
-}).then(()=>console.log('mongoDB Connected..'))
-.catch(err=>console.log(err))
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("mongoDB Connected.."))
+  .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/", (req, res) => res.send("Hello World!하이"));
+
+app.post('/register',(req, res)=>{
+  const user = new User(req.body);
+
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success:false, err})
+
+    return res.status(200).json({
+      success:true
+    })
+
+  })
+
+
+})
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
